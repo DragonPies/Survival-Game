c0671726dd87fafe5d70f481b92a51dbece3f5b9
MUKILTEO\1722453 <davewilson260@gmail.com>
2026-02-25 12:30:29 -0800

    added a bunch of scenes

---
+++ Assets/scripts/MenuButtons.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MapSelect");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void SettingsMenu()
+    {
+        Time.timeScale = 0f;
+        SceneManager.LoadScene("SettingsMenu");
+    }
+
+    public void CreditMenu()
+    { 
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("CreditMenu");
+    }
+
+    // all the levels
+
+    public void Level1()
+    {
+
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Level1");
+    }
+}
